7f51ac5b9819c387144a89758c32d725a55cfdeb
Natália Lacca <nathy.lacca@gmail.com>
2022-10-15 21:53:45 -0300

    create JS
    e ajustes finais

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,36 @@
-//passo 1
-const abas = console.log(document.querySelectorAll(".aba")); 
+// passo 1 
+const abas = document.querySelectorAll(".aba");
 
 abas.forEach(aba => {
-    //passo 2
-    aba.addEventListener("click", function() {
+    // passo 2 
+    aba.addEventListener("click", function () {
 
-        //para não executar código desnecessariamente 
-        if(aba.classList.contains("selecionado")){
+        if (aba.classList.contains("selecionado")) {
             return;
         }
 
-        //passo 3
-        const abaSelecionada = documet.querySelector(".aba.selecionado");
-        abaSelecionada.classList.remove("selecionado")
-        //passo 4
-        aba.classList.add("selecionado")
-        //passo 5
-        const informacaoSelecionada = document.querySelector(".informacao.selecionado");
-        informacaoSelecionada.classList.remove("selecionado");
-        //passo 6
-        const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
-
-        const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba) informacaoASerMostrada.classList.add("selecionado")
+        selecionarAba(aba)
+        mostrarInformacoesDaAba(aba)
     });
 });
+
+function selecionarAba(aba) {
+    // passo 3 
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+
+    // passo 4 
+    aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba) {
+    // passo 5 
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+
+    // passo 6 
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+    informacaoASerMostrada.classList.add("selecionado")
+}
